89d1dd14125417f9b938b3bc32888be1645b990a
huttonledingham <huttonledingham@gmail.com>
2026-05-28 17:08:05 -0600

    removed visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { visualizer } from "rollup-plugin-visualizer";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/EDView/' : '/',
-  plugins: [react(), visualizer({ open: true })],
+  plugins: [react()],
   esbuild: {
   },
   resolve: {
